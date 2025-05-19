bdcce159c766def50a3610b08413f22a29a08041
Marc Pont <mpont91@gmail.com>
2025-05-19 11:13:59 +0200

    fix logging bot market

--- src/bot-futures-market.ts
+++ src/bot-futures-market.ts
@@ -2,10 +2,10 @@ import { Container } from './di'
 import { Launcher } from './application/launcher'
 
 export const botFuturesMarket = (): void => {
-  console.log('Bot market running')
+  console.log('Bot futures market running')
   const launcherMarket: Launcher = Container.getLauncherFuturesMarket()
 
   launcherMarket.start().catch((error: unknown): void => {
-    console.error('Error in bot market', error)
+    console.error('Error in futures bot market', error)
   })
 }
--- src/bot-spot-market.ts
+++ src/bot-spot-market.ts
@@ -2,10 +2,10 @@ import { Container } from './di'
 import { Launcher } from './application/launcher'
 
 export const botSpotMarket = (): void => {
-  console.log('Bot market running')
+  console.log('Bot spot market running')
   const launcherMarket: Launcher = Container.getLauncherSpotMarket()
 
   launcherMarket.start().catch((error: unknown): void => {
-    console.error('Error in bot market', error)
+    console.error('Error in spot bot market', error)
   })
 }
