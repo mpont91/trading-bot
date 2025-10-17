f06b684d5c75d8d47426d2520487c387b816840f
Marc Pont <mpont91@gmail.com>
2025-10-17 12:31:35 +0200

    Refactor routes

--- src/infrastructure/web/routes.ts
+++ src/infrastructure/web/routes.ts
@@ -15,12 +15,12 @@ router.get('/uptime', (req: Request, res: Response): void => {
   res.send({ data: process.uptime() })
 })
 
-router.get('/graph/equity', getEquityGraph)
+router.get('/equity/graph', getEquityGraph)
 router.get('/commission-equity', getCommissionEquity)
 router.get('/performance', getPerformance)
 router.get('/performance/:symbol', getPerformance)
 router.get('/last-trades', getLastTrades)
 router.get('/last-trades/:symbol', getLastTrades)
-router.get('/market/graph/strategy-analysis/:symbol', getStrategyAnalysis)
+router.get('/strategy-analysis/:symbol', getStrategyAnalysis)
 
 export default router
